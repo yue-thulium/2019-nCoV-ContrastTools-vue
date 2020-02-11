afaa2b7e7168e700a0c9f5daad5c882280101899
KazaokaMari <38094006+yue-thulium@users.noreply.github.com>
2020-02-11 23:50:53 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/': {
-        target: 'http://182.92.208.18:8090',
+        target: 'http://182.92.***.18:8090',
         changeOrigin: true,
         pathRewrite: {
           '^/': ''
